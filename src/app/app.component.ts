a009ea70a2610d733141c5f341b34cd9bd8d3e26
Felipe <luisfelipeapfmendes@gmail.com>
2025-01-22 08:49:34 -0300

    feat: Adicionado menu dropdown para downloads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [[RouterOutlet], CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
+
 export class AppComponent {
   title = 'angular-project';
+  public tipos: tipoLink[] = Object.values(tipoLink) as tipoLink[];
+    listaLinks = [
+    {
+     nome: "Download 1",
+     link: "https://drive.google.com/file/d/1zStEPmJ9fhYd6W7NToEY7dD2FyNeChdC/view",
+     tipo: tipoLink.PC
+    },
+    {
+      nome: "Download 2",
+      link: "https://drive.google.com/file/d/1tyX-aM8Sqm0daPdQztT4KFy-7r6kERso/view",
+      tipo: tipoLink.PC
+     },
+     {
+      nome: "Download 3",
+      link: "https://drive.google.com/file/d/1-gRYWfVdpp44hYv-dZez6lwK8kCEUTZ_/view",
+      tipo: tipoLink.PC
+     },
+     {
+      nome: "Download 1",
+      link: "https://drive.google.com/file/d/17KYdEu95InUrZ64vPDeMJWYMScwMiPJP/view?usp=sharing",
+      tipo: tipoLink.Android
+     },
+     {
+      nome: "Download 2",
+      link: "https://www.mediafire.com/file/tu4dc3c5x5en88x/DreamScape_20241225_-1.apk/file",
+      tipo: tipoLink.Android
+     },
+     {
+      nome: "Download 1",
+      link: "https://f.fenfa88.com/PjaV",
+      tipo: tipoLink.IOS
+     },
+  ]
+
+  public retornarLink(tipo: tipoLink) {
+    if (tipo != null) {
+      return this.listaLinks.filter(v => v.tipo === tipo);
+    }
+    return [];
+  }
+
 }
+
+export enum tipoLink {
+  PC = 'PC',
+  Android = 'Android',
+  IOS = 'IOS'
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: 
+    {colors: {
+      'regal-blue': '#243c5a',
+    },},
+  },
+  plugins: [],
+}
+
